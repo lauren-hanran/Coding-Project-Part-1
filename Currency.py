c3bdaedec6ded9626970fab19c59d3f637889b6a
Lauren Hanran <lauren.hanran@my.jcu.edu.au>
2015-10-07 13:28:12 +1000

    Assignment

--- Currency.py
+++ Currency.py
@@ -1,15 +1,23 @@
 import web_utility
 
 def convert(amount, home_currency_code, location_currency_code):
-    url= str("https://www.google.com/finance/converter?a='str(amount)'&from='home_currency_code'&to='location_currency_code'")
-    result = web_utility.load_page(url_string)
+    url= str("https://www.google.com/finance/converter?a=" + str(amount) +"&from=" + home_currency_code + "&to=" + location_currency_code )
+    result = web_utility.load_page(url)
     print(result[result.index('result'):])
 
 def currency_details(country_name):
+    country_name.capitalize
+    input_file = open('currency_details.txt', mode = 'r', encoding= 'UTF-8') #open file for reading
+    line = currency_details.readline()
+
+    while country_name not in line:
+                currency_details
+
+   readline ()
+   input_file = close('currency_details.txt', )
 
 
-   input_file = open('currency_details.txt', mode = 'r', encoding= 'UTF-8')
+country_name = input("Enter Country Name: ")
 
 
-convert(1, 'AUD', 'USD')
 
