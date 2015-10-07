9a65821a7cb889cc3374e30ab4b1cdfa04fbdd13
Lauren Hanran <lauren.hanran@my.jcu.edu.au>
2015-10-07 11:40:38 +1000

    Assignment

--- Currency.py
+++ Currency.py
@@ -1,1 +1,11 @@
-__author__ = 'Lauren'
+import web_utility
+
+
+input_file = open('currency_details.txt', mode = 'r')
+
+def convert(amount, home_currency_code, location_currency_code):
+    url= "https://www.google.com/finance/converter?a=1&from=AUD&to=JPY"
+    result = web_utility.load_page(url_string)
+    print(result[result.index('result'):])
+
+def currency_details(country_name):
