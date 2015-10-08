a2855344faa5f70e9ca28da82499f3ebd13a3750
Lauren Hanran <lauren.hanran@my.jcu.edu.au>
2015-10-08 17:36:03 +1000

    Made the get_details function neat Fixed convert function

--- Currency.py
+++ Currency.py
@@ -2,33 +2,39 @@ import web_utility
 
 
 def convert(amount, home_currency_code, location_currency_code):
-    url = str("https://www.google.com/finance/converter?a=" + str(amount) + "&from=" + home_currency_code + "&to=" + location_currency_code)
-    result = web_utility.load_page(url)
-    print(result[result.index('result'):])
+    url = "https://www.google.com/finance/converter?a=" + str(amount) + "&from=" + home_currency_code + "&to=" + location_currency_code
+    try:
+        result = web_utility.load_page(url)
+        split_string = result[result.index('result'):]
+        #print(result[result.index('result'):])
+        split_string = split_string.split(">")
+        split_string = split_string[2].split(" ")
+        return print("$", split_string[0])
+
+    except:
+        return -1
 
 
 def get_details(country_name):
 
-    # country_name = str(country_name.title())
+    country_name = str(country_name.title())
     input_file = open('currency_details.txt', mode='r', encoding='UTF-8')  # open file for reading
 
     for line in input_file:
-
-        parts = line.strip().split(',')         #split into separate strings by removing ',' and white space at the end
+        parts = line.strip().split(',')
 
         if parts[0] == country_name:
             input_file.close()
             return tuple(parts)
 
     input_file.close()
-    return ()
+    return ( )
 
 
 
 
-details=get_details(input("Enter Country Name: "))
+details = get_details(input("Enter Country Name: "))
 print(details)
 
-# input_file = open('currency_details.txt', mode='r', encoding='UTF-8')
-# print (input_file.readlines(6))
-# input_file.close()
+value = convert(input("Enter amount: "), input("Enter home currency code: "), input("Enter location currency code: "))
+print(value)
