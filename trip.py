db949573c9960b359ebe60d44c68c9b852e9f62b
Lauren Hanran <lauren.hanran@my.jcu.edu.au>
2015-10-10 09:57:56 +1000

    Edited country class

--- trip.py
+++ trip.py
@@ -2,12 +2,12 @@
 # try and except
 
 class Error(Exception):
-    def __init__(self, value ):
+    def __init__(self, value):
         super(). __init__
 
     def amount(self, amount):
         if amount < 0:
-            raise AmountError("can't have negative amount of money")
+            raise AmountError("Can't have negative amount of money")
  #raise exceptions when my code generates run-time error
 
 class Country:
@@ -15,7 +15,7 @@ class Country:
      self.country_name = name
      self.country_code = country_code
      self.currency_symbol = currency_symbol
-#create initialiser method to defore 3 values for new Country object
+#create initialiser method to define 3 values for new Country object
 #create method that takes amount of money and returns money rounded to nearest cent
 #create an overloaded version of the special __str__ fn,
 #  that returns a strong counting the country details
@@ -26,7 +26,9 @@ class Country:
 class Details:
     def __init__(self, places):
         self.places = []
-        self.add_place = str(input(""))
+
+    def add_place(self, place):
+        self.places.append(place)
 
 
 
