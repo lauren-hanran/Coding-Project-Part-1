1657486b7d5026a7a89a25d8a69b5d3297f791ba
Lauren Hanran <lauren.hanran@my.jcu.edu.au>
2015-10-11 19:03:54 +1000

    Edited add method

--- Currency.py
+++ Currency.py
@@ -30,13 +30,13 @@ def get_details(country_name):  # get the country name as a string and return th
     input_file.close()
     return ()
 
-# details = get_details(input("Enter Country Name: "))
-# print(details)
-# details_2 = get_details(input("Enter Second Country Name: "))
-# print(details_2)
-#
-# value = convert(input("Enter amount: "), input("Enter home currency code: "), input("Enter location currency code: "))
-# print(value)
+details = get_details(input("Enter Country Name: "))
+print(details)
+details_2 = get_details(input("Enter Second Country Name: "))
+print(details_2)
+
+value = convert(input("Enter amount: "), input("Enter home currency code: "), input("Enter location currency code: "))
+print(value)
 
 # Test Code
 
--- trip.py
+++ trip.py
@@ -2,6 +2,7 @@ __author__ = 'Lauren Hanran'
 
 
 class Error(Exception):
+    """ not really sure wtf this does """
 
     def __init__(self, value):
         super(). __init__(value)
@@ -26,21 +27,35 @@ class Details:
     def __init__(self, locations):
         self.locations = []
 
-    def add_details(self, country_name, start_date, end_date):
-        self.locations.append(country_name)  # add country name to list
+    def add(self, country_name, start_date, end_date):
         tuple_details = (country_name, start_date, end_date)
         self.locations.append(tuple_details)
+        start_date.split('/')
+        end_date.split('/')
+        if not str.isdigit(start_date) and not str.isdigit(end_date):
+            raise Error("Date isn't formatted properly")
+        for item in start_date:
+            if len(item[0]) == 4 and len(item[1]) == 2 and len(item[2] == 2):
+                if start_date > end_date:
+                    raise ValueError("Start date is greater than end date")
+                if start_date in tuple_details:
+                    raise Error("Can't travel more than one country per day")
+            else:
+                raise Error("Date isn't formatted properly")
+
 
     def current_country(self, date_string):
-        for part in self.locations:
-            if part[1] < date_string < part[2]:
-                return part[0]
+            for part in self.locations:
+                if part[1] < date_string < part[2]:
+                    return part[0]
+
+
 
     def is_empty(self):
         return self.locations == []
 
 country_details = Country(input("enter Country name: "), input("enter Country code: "), input("enter currency symbol: ")).round_money(100.10)
-print(country_details)
+print(country_details)  # create first object of country class
 
 holiday_details = ("Australia", "19/12/2015", "31/12/2015")
-# create first object of country class
+print(holiday_details)
