7f50b23a6f255993e48f023dcbf3781d14845605
Lauren Hanran <lauren.hanran@my.jcu.edu.au>
2015-10-11 21:30:04 +1000

    Edited Details Class

--- trip.py
+++ trip.py
@@ -29,19 +29,23 @@ class Details:
 
     def add(self, country_name, start_date, end_date):
         tuple_details = (country_name, start_date, end_date)
-        self.locations.append(tuple_details)
-        start_date.split('/')
-        end_date.split('/')
-        if not str.isdigit(start_date) and not str.isdigit(end_date):
-            raise Error("Date isn't formatted properly")
+        start_list = start_date.split('/')
+        end_list = (end_date.split('/'))                # if not start_list.isdigit() and not end_list.isdigit():
+        for element in start_list:
+            if element.isdigit() == False:
+                raise Error("Start Date isn't formatted properly")
+        for element in end_list:
+            if element.isdigit() == False:
+                raise Error("End Date isn't formatted properly")
         for item in start_date:
             if len(item[0]) == 4 and len(item[1]) == 2 and len(item[2] == 2):
                 if start_date > end_date:
                     raise ValueError("Start date is greater than end date")
                 if start_date in tuple_details:
                     raise Error("Can't travel more than one country per day")
-            else:
-                raise Error("Date isn't formatted properly")
+        else:
+            self.locations.append(tuple_details)
+        return self.locations
 
 
     def current_country(self, date_string):
@@ -54,8 +58,8 @@ class Details:
     def is_empty(self):
         return self.locations == []
 
-country_details = Country(input("enter Country name: "), input("enter Country code: "), input("enter currency symbol: ")).round_money(100.10)
-print(country_details)  # create first object of country class
+# country_details = Country(input("enter Country name: "), input("enter Country code: "), input("enter currency symbol: ")).round_money(100.10)
+# print(country_details)  # create first object of country class
 details = Details()
-holiday_details = details.add("Australia", "19/12/2015", "31/12/2015")
+holiday_details = details.add("Australia", "2015/12/19", "2015/12/31")
 print(holiday_details)
